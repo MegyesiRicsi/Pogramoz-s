c99ec574287ec0110a1bb15ceca149411bad2e9f
megyesi.richard <ricsi20021114@gmail.com>
2021-10-07 13:17:45 +0200

    fálba olvas

---
+++ 20211007_2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _20211007_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_fel_Click(object sender, EventArgs e)
+        {
+            string[] beolvas = File.ReadAllLines("gyumolcsok.txt");
+            foreach (string item in beolvas)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+    }
+}
