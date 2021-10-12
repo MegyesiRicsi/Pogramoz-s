bf819935fd46064526e2b93ebd24f3ede0ec7cdc
megyesi.richard <ricsi20021114@gmail.com>
2021-10-12 13:54:02 +0200

    Órai

---
+++ 20211012/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20211012
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+           // feladat48();
+            //feladat49();
+           // feladat39();
+            feladat40();
+            Console.ReadKey();
+        }
+        static void feladat48()
+        {
+            int gen = rnd.Next(10, 51);
+            Console.Write(gen+" ");
+            while (gen!=25)
+            {
+                gen = rnd.Next(10, 51);
+                Console.Write(gen + " ");
+
+            }
+            Console.WriteLine();
+            do
+            {
+                gen = rnd.Next(10, 51);
+                Console.Write(gen + " ");
+            } while (gen!=25);
+            Console.WriteLine();
+        }
+        static void feladat49()
+        {
+            string szo = "Keresztül-kasul";
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write(szo[rnd.Next(0,szo.Length)]);
+            }
+            Console.WriteLine();
+        }
+        static void feladat39()
+        {
+            Console.WriteLine("Kérek egy egésu számot! a=");
+            int a = int.Parse(Console.ReadLine());
+            List<int> szam = new List<int>();
+            Console.Write(a+"=");
+            while (a%2==0)
+            {
+                szam.Add(2);
+                a = a / 2;
+            }
+            szam.Add(a);
+            foreach (var item in szam)
+            {
+                if (item==szam.Last())
+                {
+                    Console.Write(item);
+                }
+                else
+                {
+                    Console.Write(item+"*" );
+                }
+               
+            }
+        }
+        static void feladat40()
+        {
+            Console.WriteLine("Kérekegy egész számot");
+            int eredeti = int.Parse(Console.ReadLine());
+            int valtozik = eredeti;
+            while (valtozik>=3)
+            {
+                Console.Write(valtozik+" ");
+                valtozik -= 3;
+               
+            }
+            Console.WriteLine(valtozik);
+            Console.WriteLine($"{eredeti}={eredeti/3}*3+{valtozik}");
+        }
+    }
+}
