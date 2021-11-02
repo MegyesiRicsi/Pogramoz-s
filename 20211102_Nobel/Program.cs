53fd4f4a05929fa2df09e1c1c6f863b2eaf9e3fe
megyesi.richard <ricsi20021114@gmail.com>
2021-11-02 13:07:34 +0100

    asd

--- 20211102_Nobel/Program.cs
+++ 20211102_Nobel/Program.cs
@@ -3,46 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace _20211102_Nobel
 {
-    class Program
+    class Elethossz
     {
-        class Elethossz
+        private int Tol { get; set; }
+        private int Ig { get; set; }
+        public int ElethosszEvekben => Tol == -1 || Ig == -1 ? -1 : Ig - Tol;
+
+        public bool IsmertAzElethossz => ElethosszEvekben != -1;
+
+        public Elethossz(string SzuletesHalalozas)
         {
-            private int Tol { get; set; }
-            private int Ig { get; set; }
-            public int ElethosszEvekben => Tol == -1 || Ig == -1 ? -1 : Ig - Tol;
-
-            public bool IsmertAzElethossz => ElethosszEvekben != -1;
-
-            public Elethossz(string SzuletesHalalozas)
+            string[] m = SzuletesHalalozas.Split('-');
+            try
             {
-                string[] m = SzuletesHalalozas.Split('-');
-                try
-                {
-                    Tol = int.Parse(m[0]);
-                }
-                catch (Exception)
-                {
-                    Tol = -1;
-                }
-                try
-                {
-                    Ig = int.Parse(m[1]);
-                }
-                catch (Exception)
-                {
-                    Ig = -1;
-                }
+                Tol = int.Parse(m[0]);
+            }
+            catch (Exception)
+            {
+                Tol = -1;
+            }
+            try
+            {
+                Ig = int.Parse(m[1]);
+            }
+            catch (Exception)
+            {
+                Ig = -1;
             }
         }
+    }
+    class Nobel
+    {
+        public int ev;
+        public string nev;
+        public int eletkor;
+        public string orszagkod;
+        public Nobel(string egysor)
+        {
+            string[] darabok = egysor.Split(';');
+            ev = int.Parse(darabok[0]);
+            nev = darabok[1];
+            eletkor = new Elethossz(darabok[2]).ElethosszEvekben;
+            orszagkod = darabok[3];
+        }
+    }
+        class Program
+    {
         static void Main(string[] args)
         {
-            //Év;Név;SzületésHalálozás;Országkód
-            //1932; Adrian, lord Edgar Douglas; 1889 - 1977; GB
-            var 
         }
     }
 }
