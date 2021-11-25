17a12381c236f7ed8f29108f6249eda7c81f0e8c
megyesi.richard <ricsi20021114@gmail.com>
2021-11-25 13:20:00 +0100

    ÓR

---
+++ 20211125_HotelLiftGUI/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace _20211125_HotelLiftGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_keres = new System.Windows.Forms.Button();
+            this.btn_kiir = new System.Windows.Forms.Button();
+            this.btn_felt = new System.Windows.Forms.Button();
+            this.btn_kilep = new System.Windows.Forms.Button();
+            this.btn_Torol = new System.Windows.Forms.Button();
+            this.btn_lbir = new System.Windows.Forms.Button();
+            this.txt_cel = new System.Windows.Forms.TextBox();
+            this.txt_start = new System.Windows.Forms.TextBox();
+            this.txt_date = new System.Windows.Forms.TextBox();
+            this.txt_kartya = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_keres
+            // 
+            this.btn_keres.Location = new System.Drawing.Point(12, 294);
+            this.btn_keres.Name = "btn_keres";
+            this.btn_keres.Size = new System.Drawing.Size(75, 23);
+            this.btn_keres.TabIndex = 0;
+            this.btn_keres.Text = "Keresés";
+            this.btn_keres.UseVisualStyleBackColor = true;
+            this.btn_keres.Click += new System.EventHandler(this.Btn_keres_Click);
+            // 
+            // btn_kiir
+            // 
+            this.btn_kiir.Location = new System.Drawing.Point(93, 265);
+            this.btn_kiir.Name = "btn_kiir";
+            this.btn_kiir.Size = new System.Drawing.Size(75, 23);
+            this.btn_kiir.TabIndex = 1;
+            this.btn_kiir.Text = "Kiir";
+            this.btn_kiir.UseVisualStyleBackColor = true;
+            this.btn_kiir.Click += new System.EventHandler(this.Btn_kiir_Click);
+            // 
+            // btn_felt
+            // 
+            this.btn_felt.Location = new System.Drawing.Point(12, 265);
+            this.btn_felt.Name = "btn_felt";
+            this.btn_felt.Size = new System.Drawing.Size(75, 23);
+            this.btn_felt.TabIndex = 2;
+            this.btn_felt.Text = "Feltöltés";
+            this.btn_felt.UseVisualStyleBackColor = true;
+            this.btn_felt.Click += new System.EventHandler(this.Btn_felt_Click);
+            // 
+            // btn_kilep
+            // 
+            this.btn_kilep.Location = new System.Drawing.Point(93, 360);
+            this.btn_kilep.Name = "btn_kilep";
+            this.btn_kilep.Size = new System.Drawing.Size(75, 23);
+            this.btn_kilep.TabIndex = 3;
+            this.btn_kilep.Text = "Kilépés";
+            this.btn_kilep.UseVisualStyleBackColor = true;
+            this.btn_kilep.Click += new System.EventHandler(this.Btn_kilep_Click);
+            // 
+            // btn_Torol
+            // 
+            this.btn_Torol.Location = new System.Drawing.Point(174, 265);
+            this.btn_Torol.Name = "btn_Torol";
+            this.btn_Torol.Size = new System.Drawing.Size(75, 23);
+            this.btn_Torol.TabIndex = 4;
+            this.btn_Torol.Text = "Törlés";
+            this.btn_Torol.UseVisualStyleBackColor = true;
+            this.btn_Torol.Click += new System.EventHandler(this.Btn_Torol_Click);
+            // 
+            // btn_lbir
+            // 
+            this.btn_lbir.Location = new System.Drawing.Point(93, 294);
+            this.btn_lbir.Name = "btn_lbir";
+            this.btn_lbir.Size = new System.Drawing.Size(156, 23);
+            this.btn_lbir.TabIndex = 5;
+            this.btn_lbir.Text = "ListBox ba irás";
+            this.btn_lbir.UseVisualStyleBackColor = true;
+            this.btn_lbir.Click += new System.EventHandler(this.Btn_lbir_Click);
+            // 
+            // txt_cel
+            // 
+            this.txt_cel.Location = new System.Drawing.Point(93, 187);
+            this.txt_cel.Name = "txt_cel";
+            this.txt_cel.Size = new System.Drawing.Size(100, 20);
+            this.txt_cel.TabIndex = 6;
+            // 
+            // txt_start
+            // 
+            this.txt_start.Location = new System.Drawing.Point(93, 130);
+            this.txt_start.Name = "txt_start";
+            this.txt_start.Size = new System.Drawing.Size(100, 20);
+            this.txt_start.TabIndex = 7;
+            // 
+            // txt_date
+            // 
+            this.txt_date.Location = new System.Drawing.Point(93, 27);
+            this.txt_date.Name = "txt_date";
+            this.txt_date.Size = new System.Drawing.Size(100, 20);
+            this.txt_date.TabIndex = 8;
+            // 
+            // txt_kartya
+            // 
+            this.txt_kartya.Location = new System.Drawing.Point(93, 79);
+            this.txt_kartya.Name = "txt_kartya";
+            this.txt_kartya.Size = new System.Drawing.Size(100, 20);
+            this.txt_kartya.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 187);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Cél szint";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Kzedő szint";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Kártya szám";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Dátum";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(288, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(414, 355);
+            this.listBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 395);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_kartya);
+            this.Controls.Add(this.txt_date);
+            this.Controls.Add(this.txt_start);
+            this.Controls.Add(this.txt_cel);
+            this.Controls.Add(this.btn_lbir);
+            this.Controls.Add(this.btn_Torol);
+            this.Controls.Add(this.btn_kilep);
+            this.Controls.Add(this.btn_felt);
+            this.Controls.Add(this.btn_kiir);
+            this.Controls.Add(this.btn_keres);
+            this.Name = "Form1";
+            this.Text = "PP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_keres;
+        private System.Windows.Forms.Button btn_kiir;
+        private System.Windows.Forms.Button btn_felt;
+        private System.Windows.Forms.Button btn_kilep;
+        private System.Windows.Forms.Button btn_Torol;
+        private System.Windows.Forms.Button btn_lbir;
+        private System.Windows.Forms.TextBox txt_cel;
+        private System.Windows.Forms.TextBox txt_start;
+        private System.Windows.Forms.TextBox txt_date;
+        private System.Windows.Forms.TextBox txt_kartya;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 20211125_HotelLiftGUI/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _20211125_HotelLiftGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_kilep_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Btn_felt_Click(object sender, EventArgs e)
+        {
+            var sr = new StreamReader("lift.txt");
+            while (!sr.EndOfStream)
+            {
+                listBox1.Items.Add(sr.ReadLine());
+            }
+            sr.Close();
+        }
+
+        private void Btn_Torol_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void Btn_keres_Click(object sender, EventArgs e)
+        {
+            var datum = txt_date.Text;
+            var lista = new List<string>();
+            if (listBox1.Items.Count != 0)
+            {
+                foreach (var item in listBox1.Items)
+                {
+                    if (item.ToString().Contains(datum))
+                    {
+                        lista.Add(item.ToString());
+                    }
+                }
+                listBox1.Items.Clear();
+                foreach (var item in lista)
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+            btn_keres.Enabled = false;
+            }
+
+        private void Btn_lbir_Click(object sender, EventArgs e)
+        {
+            string datum = txt_date.Text;
+            string kartya = txt_kartya.Text;
+            string start = txt_start.Text;
+            string vege = txt_cel.Text;
+            try
+            {
+                int szam_kartya = int.Parse(kartya);
+                int szam_start = int.Parse(start);
+                int szam_cel = int.Parse(vege);
+                listBox1.Items.Add(datum + " " + kartya + " " + start + " " + vege + "\n");
+                txt_cel.Text = "";
+                txt_date.Text = "";
+                txt_kartya.Text = "";
+                txt_start.Text = "";
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Valahova nem megfelő adatot adott meg");
+            }
+        }
+
+        private void Btn_kiir_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count!=0)
+            {
+                foreach (var item in listBox1.Items)
+                {
+                    File.AppendAllText("sanyi.txt",item+ "\n");
+                 
+                }
+                MessageBox.Show("Sikeres file ba irás");
+                
+            }
+            else
+            {
+                MessageBox.Show("Üres a lista");
+            }
+           
+        }
+    }
+}
