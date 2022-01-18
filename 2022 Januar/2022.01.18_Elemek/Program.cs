cb5ac68c1546d3fdd4c15a1ddc641eeb3452720d
Megyesi Richard <ricsi20021114@gmail.com>
2022-01-18 18:02:41 +0100

    2265

--- 2022 Januar/2022.01.18_Elemek/Program.cs
+++ 2022 Januar/2022.01.18_Elemek/Program.cs
@@ -25,31 +25,92 @@ namespace _2022._01._18_Elemek
         }
         static void Main(string[] args)
         {
-            var sr = new StreamReader("felfedezesek.csv");
+            var sr = new StreamReader("felfedezesek.csv", Encoding.GetEncoding("iso_8859-2"));
             var lista = new List<Kemia>();
             var elso = sr.ReadLine();
             while (!sr.EndOfStream)
             {
                 lista.Add(new Kemia(sr.ReadLine()));
             }
-            Console.WriteLine(lista.Count());
+            Console.WriteLine($"3. feladat: Elemek szama {lista.Count()}");
             var okorban = (
                 from sor in lista
-                where sor.kor == "kor"
+                where sor.kor == "Ókor"
                 select sor);
-            foreach (var item in lista)
-            {
-                Console.WriteLine(item.kor);
-            }
-            Console.WriteLine(okorban.Count());
+            /*  foreach (var item in lista)
+              {
+                  Console.WriteLine(item.kor);
+              }
+              Console.WriteLine(okorban.Count());*/
+
             string bevegy = "";
+            int szam;
+            var tomb = new char[] { 'á', 'é', 'ó', 'ú', 'ű', 'ü', 'ő', 'ö' };
             do
-            {        
-                
-               Console.WriteLine("egy vegyjelet be");
+            {
+
+
+                Console.WriteLine("egy vegyjelet be");
                 bevegy = (Console.ReadLine());
-                                   
-            } while (bevegy.Length!=1 && bevegy.Length!=2);
+                bool success = int.TryParse(bevegy, out szam);
+                if (success)
+                {
+                    //https://docs.microsoft.com/en-us/dotnet/api/system.int32.tryparse?view=net-6.0
+                    Console.WriteLine("egy vegyjelet be");
+                    bevegy = (Console.ReadLine());
+                }
+               
+            } while (bevegy.Length != 1 && bevegy.Length != 2);
+            var keres = (
+                from sor in lista
+                where sor.vegyjel.ToLower() == bevegy.ToLower()
+                select sor);
+            if (keres.Any())
+            {
+                foreach (var item in keres)
+                {
+                    Console.WriteLine(item.vegyjel);
+                    Console.WriteLine(item.nev);
+                    Console.WriteLine(item.rendszam);
+                    Console.WriteLine(item.kor);
+                    Console.WriteLine(item.felfedezo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("ilyen nem volt");
+            }
+            var kulonb = new List<int>();
+            Console.WriteLine();
+            for (int i = 0; i < lista.Count()-1; i++)
+            {
+                if (lista[i].kor=="Ókor")
+                {
+
+                }
+                else
+                {
+                    kulonb.Add(int.Parse(lista[i+1].kor) - int.Parse(lista[i].kor));
+                }
+            }
+            Console.WriteLine(kulonb.Max());
+            /*foreach (var item in kulonb)
+             {
+                 Console.WriteLine(item);
+             }*/
+            var stat = (
+                 from sor in lista
+                 where sor.kor!="Ókor"
+                 group sor by sor.kor
+                 );
+            foreach (var item in stat)
+            {
+                if (item.Count()>3)
+                {
+                    Console.WriteLine(item.Key + "    " + item.Count());
+                }
+               
+            }
             Console.ReadKey();
         }
     }
